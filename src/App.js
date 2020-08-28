4930c6856c0ca48570696d039e7c57093a60ee35
JSGehlen <jeremygehlen570@gmail.com>
2020-08-28 14:34:17 +0300

    Finished App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import "./App.css";
+import "./styles/main.css";
+import Home from "./components/pages/HomePage/Home";
+import Services from "./components/pages/Services/Services";
+import Plans from "./components/pages/Plans/Plans";
+import SignUp from "./components/pages/SignUp/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/pages/HomePage/Home";
-
+import Footer from "./components/pages/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/plans" component={Plans} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
